abbdcb4b87eb1794372261e2d971fe49500c0694
GrupoArqui <140639842+GrupoArqui@users.noreply.github.com>
2023-08-30 11:30:08 -0500

    Update firebase.js

--- js/grupo7/firebase.js
+++ js/grupo7/firebase.js
@@ -4,13 +4,22 @@
 
 $(document).ready(function () {
 	const config = {
-		apiKey: "AIzaSyB4XD-AIzaSyDUAEKVhvQV4g9xk5uefVGAPpRJwPFZTDs",
-		authDomain: "presion-a3041.firebaseapp.com",
-		databaseURL: "https://presion-a3041-default-rtdb.firebaseio.com/",
-		projectId: "presion-a3041",
-		storageBucket: "presion-a3041.appspot.com",
-		messagingSenderId: "752009347781",
-		appId: "1:752009347781:web:7dc65ecf2f5bc204fe26ac",
+		//apiKey: "AIzaSyB4XD-AIzaSyDUAEKVhvQV4g9xk5uefVGAPpRJwPFZTDs",
+		//authDomain: "presion-a3041.firebaseapp.com",
+		//databaseURL: "https://presion-a3041-default-rtdb.firebaseio.com/",
+		//projectId: "presion-a3041",
+		//storageBucket: "presion-a3041.appspot.com",
+		//messagingSenderId: "752009347781",
+		//appId: "1:752009347781:web:7dc65ecf2f5bc204fe26ac",
+
+		apiKey: "AIzaSyA5IGd4ssUm2CmFGJRQKmqhWZRPv8VxOec",
+		authDomain: "arquitectura-grupo-7.firebaseapp.com",
+		databaseURL: "https://arquitectura-grupo-7-default-rtdb.firebaseio.com",
+		projectId: "arquitectura-grupo-7",
+		storageBucket: "arquitectura-grupo-7.appspot.com",
+		messagingSenderId: "337313638015",
+		appId: "1:337313638015:web:c0833e7cbc2c0ed74cc14a",
+		measurementId: "G-8CBNEC1ZB5"
 	};
 	firebase.initializeApp(config); //inicializamos firebase
 	var db = firebase.database();
@@ -39,7 +48,10 @@ $(document).ready(function () {
 			datos.child("hora").val(),
 			datos.child("presionSistolica").val(),
 			datos.child("presionDiastocia").val(),
+			datos.child("disk_percent").val(),
+			datos.child("received_bytes").val(), 
 		];
 		table.rows.add([dataSet]).draw();
 	});
+	
 });
